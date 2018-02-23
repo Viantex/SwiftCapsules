bf244002902d116cd982e8574649ba4056a020a3
Ahmed Elgendy <plus.elgendy@gmail.com>
2018-02-23 21:39:26 +0300

    Create SlicingCollections.swift

---
+++ collections/SlicingCollections.swift
@@ -0,0 +1,14 @@
+let collection = ["0", "01", "012",
+               "0123", "01234", "012345", "0123456", "01234567", "012345678","012345679"]
+
+// Getting the first 4 items
+collection[...3]
+// output: ["0", "01", "012", "0123"]
+
+// Getting the items which greater than the 6th item
+collection[..<5]
+// output: ["0", "01", "012", "0123", "01234"]
+
+// Getting the items wher smaller than 5 && start from 3th item
+collection[3..<5]
+
